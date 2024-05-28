8f2ccd977cbb767611d69ceac118aabc5544ab09
HaraldHaarig <132935818+HaraldHaarig@users.noreply.github.com>
2024-05-28 08:47:52 +0200

    changed Navbar

--- loreflix/src/components/Layout.jsx
+++ loreflix/src/components/Layout.jsx
@@ -1,10 +1,10 @@
 import { Outlet, Link } from "react-router-dom";
-//import "../style.css";   
+import '../Stylesheets/style.css'
 
 const Layout = () => {
     return (
         <div>
-            <nav>
+            {/* <nav>
                 <h2>Loreflix</h2>
                 <ul>
                     <li>
@@ -20,6 +20,27 @@ const Layout = () => {
                         <Link to="/SearchMovies">Search Movies</Link>
                     </li>
                 </ul>
+            </nav> */}
+            <nav class="nav">
+                <h1>Loreflix</h1>
+                {/* <a href="#" class="logo">CodingLab</a> */}
+                <ul class="nav-links">
+                <li>
+                        <Link to="/">Home</Link>
+                </li>
+                <li>
+                        <Link to="/HighestScore">Highest Score</Link>
+                </li>
+                <li>
+                        <Link to="/Trending">Trending Movies</Link>
+                </li>
+                <li>
+                        <Link to="/SearchMovies">Search Movies</Link>
+                </li>
+                </ul>
+                <div class="search-box">
+                    <input type="text" placeholder="Search here..." />
+                </div>
             </nav>
 
             <Outlet />
