38b5144189401550e49cf61bc0d0da49da6326af
binmarkoo <101064382+binmarkoo@users.noreply.github.com>
2024-05-28 08:16:56 +0200

    added Outlet to Layout

--- loreflix/src/components/Layout.jsx
+++ loreflix/src/components/Layout.jsx
@@ -2,24 +2,28 @@ import { Outlet, Link } from "react-router-dom";
 import "../style.css";
 
 const Layout = () => {
-    return(
-        <nav>
-        <h2>Loreflix</h2>
-        <ul>
-            <li>
-              <Link to="/Homepage">Home</Link>
-            </li>
-            <li>
-              <Link to="/HighestScore">Highest Score</Link>
-            </li>
-            <li>
-              <Link to="/Trending">Trending Movies</Link>
-            </li>
-            <li>
-              <Link to="/SearchMovies">Search Movies</Link>
-            </li>
-        </ul>
-      </nav>
+    return (
+        <div>
+            <nav>
+                <h2>Loreflix</h2>
+                <ul>
+                    <li>
+                        <Link to="/Homepage">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/HighestScore">Highest Score</Link>
+                    </li>
+                    <li>
+                        <Link to="/Trending">Trending Movies</Link>
+                    </li>
+                    <li>
+                        <Link to="/SearchMovies">Search Movies</Link>
+                    </li>
+                </ul>
+            </nav>
+
+            <Outlet />
+        </div>
     )
 }
 
