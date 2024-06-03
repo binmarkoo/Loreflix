89fea3227084fab6858cc94eab1dc03b8bd120e7
binmarkoo <101064382+binmarkoo@users.noreply.github.com>
2024-06-03 19:55:31 +0200

    Added GIF to NoPage

--- loreflix/src/components/NoPage.jsx
+++ loreflix/src/components/NoPage.jsx
@@ -1,6 +1,11 @@
+import myGIF from '../GIF/404.gif'
+import '../Stylesheets/image.css'
+
 const NoPage = () => {
     return(
-        <h1>404</h1>
+        <div className="gif-container">
+            <img src={myGIF} alt="My GIF" width={1080} height={720} className='gif'/>
+        </div>
     )
 }
 
